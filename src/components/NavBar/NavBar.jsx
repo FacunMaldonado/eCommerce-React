f20294a9b4609aedbc02484a4527169391da329a
FacunMaldonado <facu.sl94@gmail.com>
2022-08-24 13:21:24 -0300

    NavIcono

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 //import './nav.css'
-import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai'
+import {AiOutlineClose, AiOutlineMenu , AiOutlineShoppingCart} from 'react-icons/ai'
+
 
 
 
@@ -8,7 +9,6 @@ const NavBar = () => {
   const[nav, setNav] = useState(false);
 
   const handleNav = ()=> {
-    //setea la nav en el lado opuesto
     setNav(!nav)
   }
 
@@ -21,8 +21,9 @@ const NavBar = () => {
             <li className='p-4'>Remeras</li>
             <li className='p-4'>Contacto</li>
         </ul>
+        <AiOutlineShoppingCart className='m-10' size={50}/>
         <div onClick={handleNav} className='block md:hidden'>
-          {nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20} />}
+          {nav ? <AiOutlineClose size={30}/> : <AiOutlineMenu size={30} />}
         </div>
         <div className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r bg-[#000300] ease-in-out duration-500' : 'fixed left-[-100%]'}>
         <ul className='uppercase p-4'>
