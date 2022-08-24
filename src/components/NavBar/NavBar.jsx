86857637d8e9480fc3d8ea0f97608aae8744b040
FacunMaldonado <facu.sl94@gmail.com>
2022-08-24 00:12:44 -0300

    fix NavBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -14,7 +14,7 @@ const NavBar = () => {
 
   return (
     <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
-      <h1 className='w-full text-3xl font-bold text-[#00df9a]'>E-Commerce</h1>
+        <h1 className='w-full text-2xl font-bold text-[#00df9a]'>E-Commerce</h1>
         <ul className='hidden md:flex'>
             <li className='p-4'>Home</li>
             <li className='p-4'>Buzos</li>
@@ -25,8 +25,8 @@ const NavBar = () => {
           {nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20} />}
         </div>
         <div className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r bg-[#000300] ease-in-out duration-500' : 'fixed left-[-100%]'}>
-        <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>E-COMMERCE.</h1>
-        <ul className='pt-24 uppercase p-4'>
+        <ul className='uppercase p-4'>
+          <h1 className='w-full text-2xl font-bold text-[#00df9a] m-4'>E-COMMERCE.</h1>
             <li className='p-4 border-b'>Home</li>
             <li className='p-4 border-b'>Buzos</li>
             <li className='p-4 border-b'>Remeras</li>
