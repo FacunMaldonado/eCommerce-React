01d19a477ce073d27aa87bbdbbd8fe583e2936fa
FacunMaldonado <facu.sl94@gmail.com>
2022-08-23 23:56:36 -0300

    NavBar responsive con TailWind

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,23 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
 //import './nav.css'
 import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai'
 
 
+
 const NavBar = () => {
+  const[nav, setNav] = useState(false);
+
+  const handleNav = ()=> {
+    //setea la nav en el lado opuesto
+    setNav(!nav)
+  }
+
   return (
     <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
       <h1 className='w-full text-3xl font-bold text-[#00df9a]'>E-Commerce</h1>
-        <ul className='flex hidden'>
+        <ul className='hidden md:flex'>
             <li className='p-4'>Home</li>
             <li className='p-4'>Buzos</li>
             <li className='p-4'>Remeras</li>
             <li className='p-4'>Contacto</li>
         </ul>
-        <div>
-          <AiOutlineMenu size={20}/>
+        <div onClick={handleNav} className='block md:hidden'>
+          {nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20} />}
         </div>
-        <div className='fixed left-0 top-0 w-[60%] h-full border-r bg-[#000300]'>
-        <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>E-Commerce</h1>
+        <div className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r bg-[#000300] ease-in-out duration-500' : 'fixed left-[-100%]'}>
+        <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>E-COMMERCE.</h1>
         <ul className='pt-24 uppercase p-4'>
             <li className='p-4 border-b'>Home</li>
             <li className='p-4 border-b'>Buzos</li>
