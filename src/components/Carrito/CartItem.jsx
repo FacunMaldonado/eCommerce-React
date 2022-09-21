1f4dc4ddd6c75b36e0476ac5535775e6ecf6bc8f
FacunMaldonado <facu.sl94@gmail.com>
2022-09-21 13:28:36 -0300

    fix

--- src/components/Carrito/CartItem.jsx
+++ src/components/Carrito/CartItem.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { useCart } from '../../context/CartContext'
 
 const CartItem = ({compra}) => {
-    const{removeItem} = useCart()
-  return (
+        const{removeItem} = useCart()
+    return (
     <div style={{display:'flex', justifyContent:'space-between', alignItems:'center', padding:'2rem', width:'100%'}}>
-              <img src={compra.img} alt={compra.name} />
-              <span>{compra.name}</span>
-              <span>{compra.quantity}</span>
-              <span>${compra.price}</span>
-              <button onClick={()=>removeItem(compra.id)}>X</button>
+                <img src={compra.img} alt={compra.name} />
+                <span>{compra.name}</span>
+                <span>{compra.quantity}</span>
+                <span>${compra.price}</span>
+                <button onClick={()=>removeItem(compra.id)}>X</button>
             </div>
-  )
+    )
 }
 
 export default CartItem
