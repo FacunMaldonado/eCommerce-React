9330f6e68e7304212bae1f9512df1ff4205d8fe4
FacunMaldonado <facu.sl94@gmail.com>
2022-09-27 18:34:21 -0300

    checkout fix con sweetAlert

--- src/components/CheckOut/Checkout.jsx
+++ src/components/CheckOut/Checkout.jsx
@@ -4,11 +4,16 @@ import { useState } from 'react'
 import { db } from '../../firebase/firebase'
 import './checkout.css'
 import { useCart } from '../../context/CartContext'
+import { useNavigate } from 'react-router-dom'
+import Swal from 'sweetalert2'
 
-function Checkout() {
+
+const Checkout = () => {
     const [comprador, setComprador] = useState({})
     const [orderId, setOrderId] = useState('')
+    const [loader, setLoader] = useState(false)
     const{cart, cartTotal, clear}= useCart()
+    const navigate = useNavigate()
 
     const datosComprador = (e)=>{
         setComprador({
@@ -19,32 +24,60 @@ function Checkout() {
     
     const finalizarCompra = (e)=> {
         e.preventDefault()
-        const ventas = collection(db, "orders")
-        addDoc(ventas, {
-            comprador,
-            items: cart,
-            total: cartTotal(),
-            date: serverTimestamp()
-        })
+        if(Object.values(comprador).length !== 3){
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Debes completar todos los datos del formulario!',
+                footer: '<a href="">Why do I have this issue?</a>'
+            })
+        }else{
+            const ventas = collection(db, "orders")
+            setLoader(true)
+            Swal.fire({
+                position: 'center',
+                icon: 'success',
+                title: 'Gracias por su compra!',
+                showConfirmButton: false,
+                timer: 1500
+            })
+            addDoc(ventas, {
+                comprador,
+                items: cart,
+                total: cartTotal(),
+                date: serverTimestamp()
+            })
         .then ((res)=>{
             setOrderId(res.id)
             clear()
         })
         .catch((error)=> console.log(error))
+        .finally(()=> setLoader(false))
+        }
     }
-
-  return (
-    <div>
-        <h2>Checkout</h2>
-        <h4>Porfavor complete todos los campos</h4>
-        <form>
-            <input type="text" placeholder='Nombre y Apellido' name="name" onChange={datosComprador} onSubmit={finalizarCompra}/>
-            <input type="number"  placeholder='Telefono' name='phone' onChange={datosComprador}/>
-            <input type="email" placeholder='email@email.com' name='email' onChange={datosComprador}/>
-            <button>Finalizar Compra</button>
-        </form>
-    </div>
-  )
+    if(loader){
+        return <p>Cargando...</p>
+    }
+    return (
+        <div>
+            {!orderId
+            ?<div>
+                <h2>Checkout</h2>
+                <h4>Porfavor complete todos los campos</h4>
+                <form onSubmit={finalizarCompra}>
+                    <input type="text" placeholder='Nombre y Apellido' name="name" onChange={datosComprador}/>
+                    <input type="number"  placeholder='Telefono' name='phone' onChange={datosComprador}/>
+                    <input type="email" placeholder='email@email.com' name='email' onChange={datosComprador}/>
+                    <button>Finalizar Compra</button>
+                </form>
+            </div>
+            :
+            <div>
+                <h4>Su orden es: {orderId}</h4>
+                <button onClick={()=> navigate('/')}>Volver</button>
+            </div>}
+        </div>
+    )
 }
 
 export default Checkout
