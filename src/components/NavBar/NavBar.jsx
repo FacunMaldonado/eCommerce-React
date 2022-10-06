f07b44be79557ab120120f1eaa7ded2ea4f18f6c
FacunMaldonado <facu.sl94@gmail.com>
2022-10-06 14:52:37 -0300

    fix nav mobile

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -47,27 +47,31 @@ const NavBar = () => {
             <CartWidget />
           </NavLink>
           {/* <AiOutlineShoppingCart className='m-10' size={50}/> */}
-          <div  onClick={handleNav} className='bar block md:hidden'>
+          <div className='bar block md:hidden' onClick={() =>  handleNav()}>
             {nav ? <AiOutlineClose size={30} className='text-black'/> : <AiOutlineMenu size={30} />}
           </div>
           
-          <div className={nav ? 'navDesplegable ' : 'fixed left-[-100%]'}>
-          <ul className='uppercase p-4'>
-            <img className='TTmobile' src="https://res.cloudinary.com/dhzrx3fie/image/upload/c_scale,h_51,q_auto:low,w_427/v1664375193/CoderReact-TT/TT_1_Negro_1_b7jik5.png" alt="logo nav movile" />
-            {/* <h1 className='w-full text-2xl font-bold text-[#00df9a] m-4'>E-COMMERCE.</h1> */}
-              <li className='list_nav_mov ' onClick={() => setNav(false)}>
-                <NavLink aria-current='page' to='/categoria/Hoodie'>Hoodies</NavLink>
-              </li>
-              <li className='list_nav_mov' onClick={() => setNav(false)}>
-                <NavLink aria-current='page' to='/categoria/Remera'>Remeras</NavLink>
-              </li>
-              <li className='list_nav_mov' onClick={() => setNav(false)}>
-                <NavLink aria-current='page' to='/categoria/Accesorio'>Accesorios</NavLink>
-              </li>
-              <li className='list_nav_mov'onClick={() => setNav(false)}>
-                <NavLink aria-current='page' to='/categoria/Contacto'>Contacto</NavLink>
-              </li>
-          </ul>
+          <div className={nav ? 'navDesplegable ' : 'nav_desplegable_disable' }>
+     {/*      <div className={navMovile ? 'navDesplegable ' : null}> */}
+
+            
+            <ul className='uppercase p-4'>
+              <img className='TTmobile' src="https://res.cloudinary.com/dhzrx3fie/image/upload/c_scale,h_51,q_auto:low,w_427/v1664375193/CoderReact-TT/TT_1_Negro_1_b7jik5.png" alt="logo nav movile" />
+              {/* <h1 className='w-full text-2xl font-bold text-[#00df9a] m-4'>E-COMMERCE.</h1> */}
+                <li className='list_nav_mov ' onClick={() => setNav(false)}>
+                  <NavLink aria-current='page' to='/categoria/Hoodie'>Hoodies</NavLink>
+                </li>
+                <li className='list_nav_mov' onClick={() => setNav(false)}>
+                  <NavLink aria-current='page' to='/categoria/Remera'>Remeras</NavLink>
+                </li>
+                <li className='list_nav_mov' onClick={() => setNav(false)}>
+                  <NavLink aria-current='page' to='/categoria/Accesorio'>Accesorios</NavLink>
+                </li>
+                <li className='list_nav_mov'onClick={() => setNav(false)}>
+                  <NavLink aria-current='page' to='/categoria/Contacto'>Contacto</NavLink>
+                </li>
+            </ul>
+
           </div>
       </div>
     </header>
